c7721b52a12b9eb19b5bb25d0ccd9edea695cad9
dan1ys <barrosdanielle978@gmail.com>
2026-07-22 20:17:15 -0300

    Add files via upload

---
+++ Aula-5/conc.py
@@ -0,0 +1,49 @@
+# 1 Imprima uma mensagem de boas-vindas na tela.
+# 2 Declare uma variável booleana verdadeiro com o valor True e imprima seu tipo
+# 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+# 4 Imprima o resultado da divisão (/)de dois números inteiros de sua escolha.
+# 5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+# 6 Imprima o resultado da divisão (//)inteira de dois números inteiros de sua escolha.
+# 7 Imprima o resultado da multiplicação de 4 números decimais de sua escolha
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, imprima o dobro desse número
+# 9 Crie um sistema de cadastro com as estruturas que vc já conhece(Use apenas input, print e variavel)
+# nome, email, idade, cidade, gradução, estado civil 
+
+
+
+print ("Seja bem vindo!!")
+
+verdadeiro = True
+print(type(verdadeiro))
+
+n1 = 5.0
+n2 = 2.2 
+print(n1 * n2)
+
+int1 = 20
+int2 = 4
+print(int1 / int2)
+
+sub1 = 15
+sub2 = 7
+print(sub1 - sub2)
+
+div1 = 17
+div2 = 3
+print(div1 // div2)
+
+n1 = 1.5
+n2 = 2.0
+n3 = 3.5
+n4 = 4.0
+print(n1 * n2 * n3 * n4)
+
+numero = 10
+print(numero * 2)
+
+nome = input("Digite seu nome: ")
+email = input("Digite seu e-mail: ")
+idade = input("Digite sua idade: ")
+cidade = input("Digite sua cidade: ")
+graduacao = input("Digite sua graduação: ")
+estado_civil = input("Digite seu estado civil: ")
