f62e73ed7b58ade960f65827d6c5ed3c1009f510
dan1ys <barrosdanielle978@gmail.com>
2026-07-21 21:38:23 -0300

    Add files via upload

---
+++ Aula-4/app.py
@@ -0,0 +1,29 @@
+n1  =  int(input('>'))
+calculo = n1 ** 2
+print(calculo)
+
+
+
+Nome = input("Digite seu nome: ")
+Sobrenome = input("Digite seu sobrenome: ")
+print(Nome, Sobrenome)
+
+
+
+n2 = int(input('>>'))
+n3 = int(input('>>'))
+n1str = str(n2)
+n2str = str(n2)
+print(n2str, n1str)
+
+
+
+palavra  =  'Python'
+print(palavra, 10)
+
+
+
+frase  =  'gatinhos fofos'
+palavra  =  input('digite uma palavra ')
+print(frase, palavra)
+print(frase, palavra)
