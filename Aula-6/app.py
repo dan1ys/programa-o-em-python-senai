e0caeb3446da18b85aefdee8dac0fceae394e7d6
dan1ys <barrosdanielle978@gmail.com>
2026-07-23 19:35:19 -0300

    Add files via upload

---
+++ Aula-6/app.py
@@ -0,0 +1,35 @@
+a = 17
+b = 4
+print(a % b)
+
+a = 9
+b = 3
+resultado = (a * b) ** 2
+print(resultado)
+
+numero = 81
+print(numero ** 0.5)
+
+resultado = 20 + (3 * 4)
+print(resultado)
+
+resultado = (15 * 2) - 7
+print(resultado)
+
+resultado = 5 ** 3
+print(resultado)
+
+n1 = 17
+n2 = 21
+n3 = 25
+media = (n1 + n2 + n3) / 3
+print(media)
+
+resultado = (11 * 2) + 7
+print(resultado)
+
+resultado = ((3 * 8) - 15) / 2
+print(resultado)
+
+resultado = 2 ** 10
+print(resultado)
