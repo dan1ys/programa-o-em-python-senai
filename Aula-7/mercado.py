3b50d96e3f0de920f78fc4234c595b47d9437f3b
dan1ys <barrosdanielle978@gmail.com>
2026-07-23 21:31:22 -0300

    Add files via upload

---
+++ Aula-7/mercado.py
@@ -0,0 +1,17 @@
+lista =  list(range(2, 21))
+print(lista)
+
+lista_numeros = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(lista_numeros)
+
+primeiro_elemento = lista_numeros[3]
+print(primeiro_elemento)
+
+lista_numeros.append(9)
+print(lista_numeros)
+
+lista_numeros.remove(4)
+
+carros = ["porsche, gol, fusca"]
+lista = lista_numeros + carros
+print(lista)
